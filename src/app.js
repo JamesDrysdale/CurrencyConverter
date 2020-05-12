5c0a01f42bb616cd25ab8a84e1161103e725beac
JamesDrysdale <jamesdrysdale84@gmail.com>
2020-05-12 21:40:30 +0100

    two input fields that do nothing

--- src/app.js
+++ src/app.js
@@ -3,7 +3,23 @@ import Vue from 'vue';
 document.addEventListener('DOMContentLoaded', () => {
 
     new Vue({
-        el: "#app"
+        el: "#app",
+        data: {
+            currencies: {},
+            selectedCurrency: 0,
+        },
+        mounted() {
+            this.getCurrenices()
+        },
+        methods: {
+            getCurrenices: function(){
+                fetch("https://api.exchangeratesapi.io/latest")
+                .then(res => res.json())
+                .then(data => this.currencies = data)
+              },
+        },
 
-    })
-})
+    });
+});
+
+// Link to API: https://api.exchangeratesapi.io/latest
